eac3b89d1cf18f2d0e5fc88305118ea807034656
Natalia <nziarko1@gmail.com>
2025-03-27 12:01:08 +0100

    Custom login

--- src/main/java/com/point/hr/security/SecurityConfig.java
+++ src/main/java/com/point/hr/security/SecurityConfig.java
@@ -4,8 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -30,5 +36,21 @@ public class SecurityConfig {
         );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetMan(DataSource dataSource) {
+        JdbcUserDetailsManager theUserDetMan = new JdbcUserDetailsManager(dataSource);
+
+        theUserDetMan.setUsersByUsernameQuery("select userId, userPw, userIfActive from users where userId=?");
+
+        theUserDetMan.setAuthoritiesByUsernameQuery("select userId, urRoleId from users left join usersRoles on urUserId=userId where userId=?");
+
+        return theUserDetMan;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
