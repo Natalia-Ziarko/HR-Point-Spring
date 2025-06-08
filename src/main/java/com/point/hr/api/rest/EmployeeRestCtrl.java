6f56e6d34dcffdad3a67f10b30e75035a528c295
Apple <apple@MacBookPro.telekom.ip>
2025-06-08 10:57:30 +0200

    API response message change

--- src/main/java/com/point/hr/api/rest/EmployeeRestCtrl.java
+++ src/main/java/com/point/hr/api/rest/EmployeeRestCtrl.java
@@ -1,19 +1,17 @@
-package com.point.hr.controller;
+package com.point.hr.api.rest;
 
+import com.point.hr.controller.UtilCtrl;
 import com.point.hr.entity.Employee;
-import com.point.hr.api.ApiErrorResponse;
 import com.point.hr.api.ApiNotFoundException;
 import com.point.hr.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/employees")
 public class EmployeeRestCtrl {
 
     private final EmployeeService employeeService;
@@ -30,12 +28,12 @@ public class EmployeeRestCtrl {
         theEmployees = employeeService.findAll();
     }
 
-    @GetMapping("/employees")
+    @GetMapping("")
     public List<Employee> getEmployees() {
         return theEmployees;
     }
 
-    @GetMapping("/employee/{empId}")
+    @GetMapping("/{empId}")
     public Employee getEmployee(@PathVariable Integer empId) {
 
         Employee employee = employeeService.findById(empId);
@@ -43,7 +41,17 @@ public class EmployeeRestCtrl {
         if (employee != null) {
             return employee;
         } else {
-            throw new ApiNotFoundException("Employee not found");
+            throw new ApiNotFoundException("Employee with ID " + empId + " not found, at " + UtilCtrl.getFormattedTimeStamp(System.currentTimeMillis()));
         }
+    }
+
+    @PostMapping("")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        employee.setId(0); // INFO: Avoiding conflict - update instead of add
+
+        Employee dbEmployee = employeeService.save(employee);
+
+        return dbEmployee;
     }
 }
