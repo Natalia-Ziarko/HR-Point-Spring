d2e06fe91298936906d8383bb45c5e4d274095b2
Apple <apple@MacBookPro.telekom.ip>
2026-04-06 10:37:58 +0200

    New BaseTest

---
+++ src/test/java/com/point/hr/api/repository/BaseTest.java
@@ -0,0 +1,28 @@
+package com.point.hr.api.repository;
+
+import com.point.hr.entity.Country;
+import com.point.hr.repository.CountryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@Transactional
+public abstract class BaseTest {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @BeforeEach
+    void setUpBase() {
+        Country austria = Country.builder()
+                .id(1)
+                .name("Austria")
+                .build();
+        countryRepository.save(austria);
+    }
+}
